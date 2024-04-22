6aeb46d0f2e17c6596af1bc1b1400af1d6e47f5c
Fabio Henrique <fabiosilva5210@gmail.com>
2024-04-22 02:43:35 -0300

    add login

---
+++ script/index.js
@@ -0,0 +1,34 @@
+const hotel = "Tranquility Base Hotel & Casino"
+
+var formEvinado = document.getElementById("enviar");
+formEvinado.addEventListener("submit", function (event){
+    event.preventDefault(); //Evita o carreagamento padrao
+    var name = document.getElementById("name").value;
+    var pass = document.getElementById("pass").value;
+    
+    if (pass == "2678"){
+        alert("Bem-vindo ao " + hotel + ", "  + name + ". É um imenso prazer ter você por aqui!");
+        window.location.href = "index.html";
+         
+        // let escolha = prompt("1. 2. 3. 4.");
+        // switch(escolha){
+        //     case "1":
+        //         alert("opç 1");
+        //         break
+        //     case "2":
+        //         alert("opç 2");
+        //         break
+        //     case "3":
+        //         alert("opç 3");
+        //         break
+        //     case "4":
+        //         alert("Muito obrigado e até logo, " + name);
+        //         break
+        //     default:
+        //         alert("Nenhuma das opções escolhida.");
+        //         break
+        // }
+    }else{
+        alert("Senha incorreta");
+    }
+});
