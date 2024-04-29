39e02894410a0e360459d5307795b24c14ac183f
Fabio Henrique <fabiosilva5210@gmail.com>
2024-04-28 23:27:33 -0300

    atz

---
+++ script/pagPrinc.js
@@ -0,0 +1,25 @@
+//Fazendo a "conexão" com os botões do HTML
+var botao1 = document.getElementById("botao1");
+var botao2 = document.getElementById("botao2");
+var botao3 = document.getElementById("botao3");
+var botao4 = document.getElementById("botao4");
+var botao5 = document.getElementById("botao5");
+var botao6 = document.getElementById("botao6");
+var botao7 = document.getElementById("botao7");
+
+//Criando um ouvidor de eventos
+var form1 = document.getElementById("form1");
+var form1 = document.getElementById("");
+var form1 = document.getElementById("");
+var form1 = document.getElementById("");
+var form1 = document.getElementById("");
+var form1 = document.getElementById("");
+var form1 = document.getElementById("");
+
+botao1.addEventListener("click", function(){
+    form1.style.display = "block";
+    var inpANome = document.getElementById("inpANome").value;
+    var inpANome = document.getElementById("inpAValor").value;
+    var inpANome = document.getElementById("inpADiaria").value;
+    var inpANome = document.getElementById("inpAQuarto").value;
+});
